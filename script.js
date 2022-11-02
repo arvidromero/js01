3f716cb3e49ef350e88f217c24ee76400e901ae3
arvidromero <110180277+arvidromero@users.noreply.github.com>
2022-11-01 22:58:43 -0600

    Primera Subida

---
+++ script.js
@@ -0,0 +1,35 @@
+const inversionInicial = prompt ('Escribe la cantidad a invertir:');
+const plazoEnMeses= prompt("Plazo a meses:");
+const tasaAnual = prompt("Tasa anual:");
+const diasEnMes = (365/12);
+const tasaMensual= (tasaAnual/365)*diasEnMes;
+
+
+
+for(i=1 ; i <= plazoEnMeses ; i++){
+    
+    var interesAculumado;
+    var inversionAcumulada;
+
+
+
+    
+
+}
+
+
+function tasaDelPeriodo(tasaAnual){
+     return (30*tasaAnual)/360
+}
+
+
+function calcularInteres(importe , tasaDiaria, dias){
+     
+}
+
+
+
+
+
+
+
