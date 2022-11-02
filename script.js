f02be3fe8cce6b7190b6fa15a9a9fb7f3d41cab1
arvidromero <110180277+arvidromero@users.noreply.github.com>
2022-11-01 23:36:28 -0600

    Primera entrega
    Archivos de la primera entrega

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 const inversionInicial = prompt ('Escribe la cantidad a invertir:');
 const plazoEnMeses= prompt("Plazo a meses:");
 const tasaAnual = prompt("Tasa anual:");
-const diasEnMes = (365/12);
-const tasaMensual= (tasaAnual/365)*diasEnMes;
+
+var interesAculumado=0;
+var interesGenerado;
 
 
+if (inversionInicial >= 0){
+    if (plazoEnMeses >=0){
+        if (tasaAnual >=0){
+            console.log('Desglose de intereses:')
+            console.log('----------------------------------')
+            
+            
+            for(i=1 ; i <= plazoEnMeses ; i++){
+                                
+                interesGenerado=calculoRendimiento(inversionInicial,tasaAnual,30)
+                console.log('Interes mes ' + i + ': $ ' + interesGenerado);
+                interesAculumado += interesGenerado
+                
+            }
+            
+            console.log('----------------------------------')
+            console.log('Interes Acumulado: $' + interesAculumado);
+            console.log('Inversion + Interes: $' + (parseFloat(inversionInicial) + parseFloat(interesAculumado)) );
 
-for(i=1 ; i <= plazoEnMeses ; i++){
-    
-    var interesAculumado;
-    var inversionAcumulada;
 
-
-
-    
-
+            function calculoRendimiento(importe, tasa , dias){
+                return Math.round(importe * (((dias*tasa)/360) /100)); 
+            }
+            500
+        }
+    }
 }
 
 
-function tasaDelPeriodo(tasaAnual){
-     return (30*tasaAnual)/360
-}
 
 
-function calcularInteres(importe , tasaDiaria, dias){
-     
-}
+
 
 
 
