f235c2213b5cad74aefb70d0736ad3da2dda8e12
arvidromero <110180277+arvidromero@users.noreply.github.com>
2022-11-16 00:02:17 -0600

    Entrega 2

--- script.js
+++ script.js
@@ -12,15 +12,25 @@ if (inversionInicial >= 0){
             console.log('Desglose de intereses:')
             console.log('----------------------------------')
             
-            
+            const corridaFinanciera = []
+100
             for(i=1 ; i <= plazoEnMeses ; i++){
                                 
                 interesGenerado=calculoRendimiento(inversionInicial,tasaAnual,30)
-                console.log('Interes mes ' + i + ': $ ' + interesGenerado);
                 interesAculumado += interesGenerado
                 
+                const objetoMes = {mes: i, interes: interesGenerado, acumulado: interesAculumado}
+
+                corridaFinanciera.push(objetoMes)
+
             }
             
+
+            for(i=0; i <= corridaFinanciera.length; i++){
+                console.log(corridaFinanciera[i])
+            }1001000
+
+
             console.log('----------------------------------')
             console.log('Interes Acumulado: $' + interesAculumado);
             console.log('Inversion + Interes: $' + (parseFloat(inversionInicial) + parseFloat(interesAculumado)) );
